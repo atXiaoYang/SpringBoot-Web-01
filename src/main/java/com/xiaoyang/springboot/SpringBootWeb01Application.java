43ba864ebcc3ff3457b0d7d9e20a15dd6839604b
xiaoyang <xiaoyang@qq.com>
2022-11-01 15:53:32 +0800

    idea gitee first commit

--- src/main/java/com/xiaoyang/springboot/SpringBootWeb01Application.java
+++ src/main/java/com/xiaoyang/springboot/SpringBootWeb01Application.java
@@ -15,6 +15,7 @@ public class SpringBootWeb01Application {
         System.out.println("hello hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("gitee push test!");
     }
 
 }
